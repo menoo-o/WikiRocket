d724417fda4b1ae8a3e212ef244328000e465694
menoo-o <menosuper6@gmail.com>
2024-10-28 13:49:22 +0500

    last commit

--- app/components/Search.tsx
+++ app/components/Search.tsx
@@ -1,13 +1,13 @@
 'use client'
 
-import { useState, FormEvent } from 'react'
-import { useRouter } from 'next/navigation'
+import { useState, FormEvent } from "react"
+import { useRouter } from "next/navigation"
 
 export default function Search() {
     const [search, setSearch] = useState('')
     const router = useRouter()
-    
-    const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
+
+    const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
         e.preventDefault()
         setSearch('')
         router.push(`/${search}/`)
@@ -15,12 +15,12 @@ export default function Search() {
 
     return (
         <form className="w-50 flex justify-center md:justify-between" onSubmit={handleSubmit}>
-            <input 
-                type="text" 
+            <input
+                type="text"
                 value={search}
                 onChange={(e) => setSearch(e.target.value)}
                 className="bg-white p-2 w-80 text-xl rounded-xl"
-                placeholder="Search" 
+                placeholder="Search"
             />
             <button className="p-2 text-xl rounded-xl bg-slate-300 ml-2 font-bold">
                 🚀
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,10 @@
-import {Inter} from 'next/font/google'
-const inter = Inter({subsets: ['latin']})
+import { Inter } from 'next/font/google'
+const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
     <main className={inter.className}>
-      
+
     </main>
-  );
+  )
 }
